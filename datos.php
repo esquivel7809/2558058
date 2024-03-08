38eb5ee81f5b34785b2df60b2f14845b325bd0fa
viuche3 <sebastianviuche2020@gmail.com>
2024-03-08 13:57:11 -0500

    trabajo finalizado

--- datos.php
+++ datos.php
@@ -9,13 +9,13 @@
     $sql="SELECT user.doc, user.name FROM asig_transv 
     INNER JOIN user ON asig_transv.doc_trans = user.doc 
     INNER JOIN transversal ON asig_transv.id_transv = transversal.id_transv 
-    WHERE user.id_tip_user = 3 
+    WHERE user.id_tip_user = 1
     AND transversal.id_transv = '$trans'
     ORDER BY transversal.transversal ";
 
     $result=mysqli_query($conexion,$sql);
 	$cadena="<label>Instructor</label><br> 
-			<select id='docu' name='name'>";
+			<select id='doc' name='name'>";
 
 	while ($ver=mysqli_fetch_row($result)) {
 		$cadena=$cadena.'<option value='.$ver[0].'>'.utf8_encode($ver[1]).'</option>';
